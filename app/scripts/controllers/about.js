0972d3155a4fd63b0d3d69a3d92bd087bc8039c2
Amy Bakameyer <bymak87@gmail.com>
2015-11-11 22:10:03 -0600

    set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keystokeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the keystokeApp
+ */
+angular.module('keystokeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name keystokeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the keystokeApp
+ */
+angular.module('keystokeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
